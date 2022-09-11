d604bd3c230ee9fcad47911ce3cf649017fe98c3
Hector Olea <imacako@gmail.com>
2022-09-10 22:19:57 -0500

    Initial commit

---
+++ src/test/java/dev/macako/homeinventory/homeinventory/HomeInventoryApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.macako.homeinventory.homeinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class HomeInventoryApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
+}
