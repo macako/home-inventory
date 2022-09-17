f9239d23111fe3eb0b1ad77c4f037b504fc91b91
Hector Olea <imacako@gmail.com>
2022-09-16 20:48:37 -0500

    created config server

---
+++ home-inventory-config-server/src/main/java/dev/macako/homeinventory/configserver/homeinventoryconfigserver/HomeInventoryConfigServerApplication.java
@@ -0,0 +1,14 @@
+package dev.macako.homeinventory.configserver.homeinventoryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HomeInventoryConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomeInventoryConfigServerApplication.class, args);
+  }
+}
---
+++ home-inventory-config-server/src/test/java/dev/macako/homeinventory/configserver/homeinventoryconfigserver/HomeInventoryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.macako.homeinventory.configserver.homeinventoryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
