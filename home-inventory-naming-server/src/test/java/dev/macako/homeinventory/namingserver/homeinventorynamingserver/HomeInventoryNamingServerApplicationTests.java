5ff32ce566bad65fe34536764aa90619b39aaf9e
Hector Olea <imacako@gmail.com>
2022-09-16 20:25:42 -0500

    Moved home inventory to new folder

---
+++ home-inventory-naming-server/src/test/java/dev/macako/homeinventory/namingserver/homeinventorynamingserver/HomeInventoryNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.macako.homeinventory.namingserver.homeinventorynamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
