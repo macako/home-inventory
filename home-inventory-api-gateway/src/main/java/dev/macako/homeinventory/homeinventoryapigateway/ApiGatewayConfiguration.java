959e5a8b293582796a295622559771172adc331a
Hector Olea <imacako@gmail.com>
2022-09-23 20:37:08 -0500

    Added api gateway project

---
+++ home-inventory-api-gateway/src/main/java/dev/macako/homeinventory/homeinventoryapigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,33 @@
+package dev.macako.homeinventory.homeinventoryapigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+  @Bean
+  public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            p ->
+                p.path("/get")
+                    .filters(
+                        f ->
+                            f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                    .uri("http://httpbin.org:80"))
+        .route(p -> p.path("/items/**").uri("lb://home-inventory-item-service"))
+        .route(p -> p.path("/users/**").uri("lb://home-inventory-user-service"))
+        .route(
+            p ->
+                p.path("/v2/items/**")
+                    .filters(
+                        f -> f.rewritePath("/v2/items/(?<segment>.*)", "/items-feign/${segment}"))
+                    .uri("lb://home-inventory-item-service"))
+        .build();
+  }
+}
---
+++ home-inventory-api-gateway/src/test/java/dev/macako/homeinventory/homeinventoryapigateway/HomeInventoryApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.macako.homeinventory.homeinventoryapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
